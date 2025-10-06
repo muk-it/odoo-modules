d49649c4aec2a760224d63c8cbc9746fb27f061e
Mathias Markl <mathias.markl@mukit.at>
2025-10-06 11:23:41 +0200

    update

--- muk_web_appsbar/__manifest__.py
+++ muk_web_appsbar/__manifest__.py
@@ -5,11 +5,7 @@
         This module adds a sidebar to the main screen. The sidebar has a list
         of all installed apps similar to the home menu to ease navigation.
     ''',
-<<<<<<< HEAD
-    'version': '17.0.1.1.2', 
-=======
-    'version': '17.0.1.1.2',
->>>>>>> b7226b9058c452bcb517a0df9088e2ba799438bb
+    'version': '17.0.1.1.3', 
     'category': 'Tools/UI',
     'license': 'LGPL-3', 
     'author': 'MuK IT',
